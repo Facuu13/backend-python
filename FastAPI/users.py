e68925a9d9634051c4aa0e2a63b6f7b8f71d270a
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2023-12-15 12:04:14 -0300

    path y query

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -6,14 +6,15 @@ app = FastAPI()
 # comando para levantar el servidor: uvicorn users:app --reload
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
     
-users_list = [User(name="Facu",surname="villa",url="google.com",age=28),
-            User(name="alan",surname="villa",url="google.com",age=25),
-            User(name="gina",surname="villa",url="google.com",age=23)]
+users_list = [User(id=1,name="Facu",surname="villa",url="google.com",age=28),
+            User(id=2,name="alan",surname="villa",url="google.com",age=25),
+            User(id=3,name="gina",surname="villa",url="google.com",age=23)]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -23,3 +24,22 @@ async def usersjson():
 @app.get("/users")
 async def users():
     return users_list
+
+#path
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+
+#query
+#http://127.0.0.1:8000/userquery/?id=1    
+@app.get("/userquery/")
+async def user(id: int):
+    return search_user(id)
+
+
+def search_user(id:int):
+    users= filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error":"No se ha encontrado un usuario"}
