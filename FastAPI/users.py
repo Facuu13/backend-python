338f223ee9e457575b99152a75c1e8e5f263f4a2
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2023-12-18 18:24:58 -0300

    se agregaron los metodos post, put y delete

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -43,3 +43,32 @@ def search_user(id:int):
         return list(users)[0]
     except:
         return {"error":"No se ha encontrado un usuario"}
+    
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error":"El usuario ya existe"}
+    else:
+        users_list.append(user)
+        
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+            
+    if not found:
+        return {"error":"No se ha actualizado el usuario"}
+    
+@app.delete("/user/{id}")
+async def user(id : int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+            
+    if not found:
+        return {"error":"No se ha eliminado el usuario"}
