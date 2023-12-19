b2f799465ad4e2d31935ae4b08ff62f3a08242db
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2023-12-19 17:59:20 -0300

    exponemos recursos estaticos

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
-
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 # comando para levantar el servidor: uvicorn main:app --reload
 
